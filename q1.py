8fd0aaf928be277f4268f9abdadceae86c6ebdbf
flashmaestro <masterofflash@nate.com>
2018-09-22 21:24:04 +0900

    Training 01

---
+++ q1.py
@@ -0,0 +1,17 @@
+# 어떻게 원하는 내용을 출력할 것인가?
+"""
+name = "Jake"
+print(3.141592, "Hi", name)
+"""
+
+# 어떻게 원하는 내용을 입력받을 것인가?
+# + 변수의 개념
+"""
+What is your name? Jake
+Hello, Jake, nice to meet you!
+"""
+
+name = input("What is your name? ")
+print("Hello,", name, ", nice to meet you!")
+
+# format string
